701edc71fb36767cca579c2b36cb44fdc07633ec
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-01-17 10:41:47 -0500

    add long.h

--- echion/strings.h
+++ echion/strings.h
@@ -7,14 +7,12 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 #include <unicodeobject.h>
-#if PY_VERSION_HEX >= 0x030c0000
-#include <internal/pycore_long.h>
-#endif
 
 #include <cstdint>
 #include <exception>
 #include <string>
 
+#include <echion/long.h>
 #include <echion/vm.h>
 
 class StringError : public std::exception
@@ -22,14 +20,6 @@ class StringError : public std::exception
     const char *what() const noexcept override
     {
         return "StringError";
-    }
-};
-
-class LongError : public std::exception
-{
-    const char *what() const noexcept override
-    {
-        return "LongError";
     }
 };
 
@@ -80,42 +70,6 @@ pyunicode_to_utf8(PyObject *str_addr)
 
     return dest;
 }
-
-
-// ----------------------------------------------------------------------------
-#if PY_VERSION_HEX >= 0x030c0000
-
-static long long
-pylong_to_llong(PyObject *long_addr)
-{
-  // Only used to extract a task-id on Python 3.12, omits overflow checks
-  PyLongObject long_obj;
-  long long ret = 0;
-
-  if (copy_type(long_addr, long_obj))
-    throw LongError();
-
-  if (!PyLong_CheckExact(&long_obj))
-    throw LongError();
-
-  if (_PyLong_IsCompact(&long_obj)) {
-    ret = (long long)_PyLong_CompactValue(&long_obj);
-  } else {
-    // If we're here, then we need to iterate over the digits
-    // We might overflow, but we don't care for now
-    int sign = _PyLong_NonCompactSign(&long_obj);
-    Py_ssize_t i = _PyLong_DigitCount(&long_obj);
-    while (--i >= 0) {
-      ret <<= PyLong_SHIFT;
-      ret |= long_obj.long_value.ob_digit[i];
-    }
-    ret *= sign;
-  }
-
-  return ret;
-}
-#endif
-
 
 // ----------------------------------------------------------------------------
 
