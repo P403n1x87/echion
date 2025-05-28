d32c8dc5a760c455fb957f46072a20ed7e959747
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-05-28 15:33:26 -0400

    chore: upgrade and pin actions (#121)
    * chore: upgrade and pin actions
    
    * install libtool
    
    * install po4a
    
    * print
    
    * use yum
    
    * use y
    
    * no-po4a
    
    * oh im confused
    
    * install libtool
    
    * try this
    
    * build for all

--- .github/workflows/cibw_before_all.sh
+++ .github/workflows/cibw_before_all.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
+set -ex
+
+# musllinux doesn't have libtool by default
+if command -v apk &> /dev/null; then
+  apk add libtool
+fi
+
 . scripts/build_libunwind.sh
 
-sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
-sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+if command -v yum &> /dev/null; then
+    sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+    sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+fi
 
 # Install xz
-yum makecache
-yum install -y gettext-devel
+if command -v yum &> /dev/null; then
+    yum makecache
+    yum install -y gettext-devel
+elif command -v apk &> /dev/null; then
+    apk add gettext-dev
+fi
 
 git clone https://git.tukaani.org/xz.git
 cd xz
-./autogen.sh
+./autogen.sh --no-po4a
 ./configure CFLAGS='-fPIC' CXXFLAGS='-fPIC'
 make
 make install
