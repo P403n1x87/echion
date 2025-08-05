f0ed22589b85ba7e7fef923c9504befb5b6c97c0
Gabriele N. Tornetta <P403n1x87@users.noreply.github.com>
2025-08-05 17:13:12 +0100

    perf: interpreter state field read (#123)
    The interpreter state is a large structure that spans across
    multiple memory pages. Remote reads of this structure in a single
    chunk suffer a performance penalty that can be avoided because we
    only need a bunch of fields from it. It is therefore more
    efficient to read the single fields than all the pages backing the
    full structure.

--- echion/interp.h
+++ echion/interp.h
@@ -20,10 +20,34 @@
 #include <echion/state.h>
 #include <echion/vm.h>
 
-static void for_each_interp(std::function<void(PyInterpreterState* interp)> callback)
+
+class InterpreterInfo
 {
-    PyInterpreterState interp;
-    for (PyInterpreterState* interp_addr = runtime->interpreters.head;
-         !copy_type(interp_addr, interp); interp_addr = interp.next)
-        callback(&interp);
+public:
+    int64_t id = 0;
+    void* tstate_head = NULL;
+    void* next = NULL;
+};
+
+static void for_each_interp(std::function<void(InterpreterInfo& interp)> callback)
+{
+    InterpreterInfo interpreter_info = {0};
+
+    for (char* interp_addr = (char*)runtime->interpreters.head; interp_addr != NULL;
+         interp_addr = (char*)interpreter_info.next)
+    {
+        if (copy_type(interp_addr + offsetof(PyInterpreterState, id), interpreter_info.id))
+            continue;
+#if PY_VERSION_HEX >= 0x030b0000
+        if (copy_type(interp_addr + offsetof(PyInterpreterState, threads.head),
+#else
+        if (copy_type(interp_addr + offsetof(PyInterpreterState, tstate_head),
+#endif
+                      interpreter_info.tstate_head))
+            continue;
+        if (copy_type(interp_addr + offsetof(PyInterpreterState, next), interpreter_info.next))
+            continue;
+
+        callback(interpreter_info);
+    };
 }
