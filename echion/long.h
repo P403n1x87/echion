9b7e76cf922b7dcbed5c13dd6adf197dff135bb4
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-09-12 16:55:28 -0400

    format

--- echion/long.h
+++ echion/long.h
@@ -9,14 +9,14 @@
 // Note: Even if use the right PYLONG_BITS_IN_DIGIT that is specified in the
 // Python we use to build echion, it can be different from the Python that is
 // used to run the program.
-#if PYLONG_BITS_IN_DIGIT ==  30
+#if PYLONG_BITS_IN_DIGIT == 30
 typedef uint32_t digit;
 #elif PYLONG_BITS_IN_DIGIT == 15
 typedef unsigned short digit;
 #else
 #error "Unsupported PYLONG_BITS_IN_DIGIT"
-#endif // PYLONG_BITS_IN_DIGIT
-#endif // PY_VERSION_HEX >= 0x030c0000
+#endif  // PYLONG_BITS_IN_DIGIT
+#endif  // PY_VERSION_HEX >= 0x030c0000
 
 #include <exception>
 
