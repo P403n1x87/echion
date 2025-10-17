2c792b2561981053a5ee80129980945d6d1f5f8c
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-10-17 10:59:33 +0200

    ci: mark memory test xfail (#161)
    * mark memory test xfail
    
    * format

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -1,6 +1,9 @@
+import pytest
+
 from tests.utils import DataSummary, run_target
 
 
+@pytest.mark.xfail(reason="Memory profiling is flaky")
 def test_memory():
     result, data = run_target("target_mem", "-m")
     assert result.returncode == 0, result.stderr.decode()
@@ -14,4 +17,6 @@ def test_memory():
     expected_nthreads = 1
     assert summary.nthreads == expected_nthreads
 
-    assert summary.query("0:MainThread", (("<module>", 25), ("leak", 21))) is not None, summary.threads["0:MainThread"]
+    assert (
+        summary.query("0:MainThread", (("<module>", 25), ("leak", 21))) is not None
+    ), summary.threads["0:MainThread"]
