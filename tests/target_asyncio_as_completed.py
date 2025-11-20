63062b8af61fb021033fcc18d4655e6ec76a36c6
Thomas Kowalski <thomas.kowalski@datadoghq.com>
2025-11-20 16:52:16 +0100

    feat: track asyncio.as_completed

--- tests/target_asyncio_as_completed.py
+++ tests/target_asyncio_as_completed.py
@@ -1,13 +1,31 @@
+import random
 import asyncio
 
 
-async def inner():
-    await asyncio.sleep(0.5)
+async def other(t: float):
+    await asyncio.sleep(t)
 
 
-async def outer():
-    as_completed = next(asyncio.as_completed([inner()]))
-    await as_completed
+async def wait_and_return_delay(t: float) -> float:
+    await other(t)
+    return t
 
 
-asyncio.run(outer())
+async def main() -> None:
+    # Create a mix of Tasks and Coroutines
+    futures = [
+        asyncio.create_task(wait_and_return_delay(i / 10))
+        if i % 2 == 0
+        else wait_and_return_delay(i / 10)
+        for i in range(10)
+    ]
+    random.shuffle(futures)
+    result: list[float] = []
+    for future in asyncio.as_completed(futures):
+        result.append(await future)
+
+    assert sorted(result) == result
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
